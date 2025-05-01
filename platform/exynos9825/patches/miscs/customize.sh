0f576138635ed88b1a8846c1c0e6953a2e72f429
Ocin4ever <winicoco24@gmail.com>
2025-05-01 15:27:56 +0200

    platform: exynos9825: adapt misc for 9825 and for e2s

--- platform/exynos9825/patches/miscs/customize.sh
+++ platform/exynos9825/patches/miscs/customize.sh
@@ -1,25 +1,16 @@
-echo "Disabling A/B"
-SET_PROP "product" "ro.product.ab_ota_partitions" --delete
-
 echo "Disabling UFFD GC"
 SET_PROP "product" "ro.dalvik.vm.enable_uffd_gc" "false"
 
 echo "Disabling encryption"
 # Encryption
-LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos9820")
-sed -i "${LINE}s/,fileencryption=ice//g" "$WORK_DIR/vendor/etc/fstab.exynos9820"
+LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos9825")
+sed -i "${LINE}s/,fileencryption=ice//g" "$WORK_DIR/vendor/etc/fstab.exynos9825"
 
 # ODE
-sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.exynos9820"
+sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.exynos9825"
 
 echo "Enabling updateable APEX images"
 SET_PROP "vendor" "ro.apex.updatable" "true"
-
-
-echo "Setting /data to F2FS"
-FROM="noatime,nosuid,nodev,noauto_da_alloc,discard,journal_checksum,data=ordered,errors=panic"
-TO="noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678"
-sed -i -e "${LINE}s/ext4/f2fs/g" -e "${LINE}s/$FROM/$TO/g" "$WORK_DIR/vendor/etc/fstab.exynos9820"
 
 echo "Disabling A2DP Offload"
 SET_PROP "system" persist.bluetooth.a2dp_offload.disabled "true"
