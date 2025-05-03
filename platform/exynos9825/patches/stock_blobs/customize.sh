3ca953fa7a0683525ab917f3bf73f5cbafe02431
Ocin4ever <winicoco24@gmail.com>
2025-05-03 16:09:38 +0200

    all: use p3s soundbooster instead of stock

--- platform/exynos9825/patches/stock_blobs/customize.sh
+++ platform/exynos9825/patches/stock_blobs/customize.sh
@@ -2,8 +2,8 @@
 # N10 Series -> SoundBooster 1050
 echo "Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 echo "Replacing GameDriver"
 DELETE_FROM_WORK_DIR "system" "system/priv-app/GameDriver-EX2400"
