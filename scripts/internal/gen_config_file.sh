salvogiangri/UN1CA

scripts: gen_config_file: refactor to match recent changes in build system
Change-Id: I5e365c3f64a958eedb56d76c689b1a69a56104db

scripts/internal/gen_config_file.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2023 Salvo Giangreco
+# Copyright (C) 2025 Salvo Giangreco
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,132 +16,145 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC1090,SC1091
-
-set -Ee
-
 # [
-trap 'rm -f $OUT_DIR/config.sh' ERR
+source "$SRC_DIR/scripts/utils/common_utils.sh" || exit 1
 
-GET_OFFICIAL_STATUS()
+trap '[ $? -ne 0 ] && rm -f "$OUT_DIR/config.sh"' EXIT
+
+GET_BUILD_VAR()
 {
-    local USES_UNICA_CERT=false
-
-    if [ -f "$SRC_DIR/security/unica_platform.pk8" ]; then
-        openssl ec -pubout -in "$SRC_DIR/security/unica_platform.pk8" -out "$OUT_DIR/unica_platform.pub" &> /dev/null \
-            || touch "$OUT_DIR/unica_platform.pub"
-        if cmp -s "$SRC_DIR/security/unica_platform.pub" "$OUT_DIR/unica_platform.pub"; then
-            USES_UNICA_CERT=true
+    if [ "$2" ]; then
+        if [ ! "${!1}" ]; then
+            echo "${1}=\"${2}\""
+            return 0
         fi
-        rm -f "$OUT_DIR/unica_platform.pub"
+    else
+        _CHECK_NON_EMPTY_PARAM "$1" "${!1}" || exit 1
+    fi
+
+    echo "${1}=\"${!1}\""
+    return 0
+}
+
+IS_UNICA_CERT_AVAILABLE()
+{
+    local PLATFORM_KEY_SHA1="5b0eb951718acc596370dabab83f546e779b21dc"
+    local OTA_KEY_SHA1="681aa9d28fe5fc60be8c25dc5f26a73ec3d6fb46"
+
+    local USES_UNICA_CERT="false"
+    if [[ "$(sha1sum "$SRC_DIR/security/unica_platform.pk8" 2> /dev/null | cut -d " " -f 1)" == "$PLATFORM_KEY_SHA1" ]] && \
+            [[ "$(sha1sum "$SRC_DIR/security/unica_ota.pk8" 2> /dev/null | cut -d " " -f 1)" == "$OTA_KEY_SHA1" ]]; then
+        USES_UNICA_CERT="true"
     fi
 
     echo "$USES_UNICA_CERT"
 }
-
-GEN_CONFIG_FILE()
-{
-    if [ -f "$OUT_DIR/config.sh" ]; then
-        echo "config.sh already exists. Regenerating..."
-        rm -f "$OUT_DIR/config.sh"
-    fi
-
-    {
-        echo "# Automatically generated by unica/scripts/internal/gen_config_file.sh"
-        echo "ROM_IS_OFFICIAL=\"$(GET_OFFICIAL_STATUS)\""
-        echo "ROM_VERSION=\"${ROM_VERSION:?}\""
-        echo "ROM_CODENAME=\"${ROM_CODENAME:?}\""
-        echo "ROM_BUILD_TIMESTAMP=\"$(date '+%s')\""
-        echo "SOURCE_FIRMWARE=\"${SOURCE_FIRMWARE:?}\""
-        if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            echo "SOURCE_EXTRA_FIRMWARES=\"$( IFS=:; printf '%s' "${SOURCE_EXTRA_FIRMWARES[*]}" )\""
-        else
-            echo "SOURCE_EXTRA_FIRMWARES=\"\""
-        fi
-        echo "SOURCE_API_LEVEL=\"${SOURCE_API_LEVEL:?}\""
-        echo "SOURCE_PRODUCT_FIRST_API_LEVEL=\"${SOURCE_PRODUCT_FIRST_API_LEVEL:?}\""
-        echo "SOURCE_VNDK_VERSION=\"${SOURCE_VNDK_VERSION:?}\""
-        echo "TARGET_NAME=\"${TARGET_NAME:?}\""
-        echo "TARGET_CODENAME=\"${TARGET_CODENAME:?}\""
-        if [ "${#TARGET_ASSERT_MODEL[@]}" -ge 1 ]; then
-            echo "TARGET_ASSERT_MODEL=\"$( IFS=:; printf '%s' "${TARGET_ASSERT_MODEL[*]}" )\""
-        else
-            echo "TARGET_ASSERT_MODEL=\"\""
-        fi
-        echo "TARGET_FIRMWARE=\"${TARGET_FIRMWARE:?}\""
-        if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            echo "TARGET_EXTRA_FIRMWARES=\"$( IFS=:; printf '%s' "${TARGET_EXTRA_FIRMWARES[*]}" )\""
-        else
-            echo "TARGET_EXTRA_FIRMWARES=\"\""
-        fi
-        echo "TARGET_API_LEVEL=\"${TARGET_API_LEVEL:?}\""
-        echo "TARGET_PRODUCT_FIRST_API_LEVEL=\"${TARGET_PRODUCT_FIRST_API_LEVEL:?}\""
-        echo "TARGET_VNDK_VERSION=\"${TARGET_VNDK_VERSION:?}\""
-        echo "TARGET_SINGLE_SYSTEM_IMAGE=\"${TARGET_SINGLE_SYSTEM_IMAGE:?}\""
-        echo "TARGET_OS_FILE_SYSTEM=\"${TARGET_OS_FILE_SYSTEM:?}\""
-        echo "TARGET_BOOT_DEVICE_PATH=\"${TARGET_BOOT_DEVICE_PATH:=/dev/block/bootdevice/by-name}\""
-        echo "TARGET_INCLUDE_PATCHED_VBMETA=\"${TARGET_INCLUDE_PATCHED_VBMETA:=false}\""
-        echo "TARGET_KEEP_ORIGINAL_SIGN=\"${TARGET_KEEP_ORIGINAL_SIGN:=false}\""
-        echo "TARGET_SUPER_PARTITION_SIZE=\"${TARGET_SUPER_PARTITION_SIZE:?}\""
-        echo "SOURCE_SUPER_GROUP_NAME=\"${SOURCE_SUPER_GROUP_NAME:?}\""
-        echo "TARGET_SUPER_GROUP_NAME=\"${TARGET_SUPER_GROUP_NAME:=$SOURCE_SUPER_GROUP_NAME}\""
-        echo "TARGET_SUPER_GROUP_SIZE=\"${TARGET_SUPER_GROUP_SIZE:?}\""
-        echo "SOURCE_HAS_SYSTEM_EXT=\"${SOURCE_HAS_SYSTEM_EXT:?}\""
-        echo "TARGET_HAS_SYSTEM_EXT=\"${TARGET_HAS_SYSTEM_EXT:?}\""
-        echo "SOURCE_AUDIO_SUPPORT_ACH_RINGTONE=\"${SOURCE_AUDIO_SUPPORT_ACH_RINGTONE:?}\""
-        echo "TARGET_AUDIO_SUPPORT_ACH_RINGTONE=\"${TARGET_AUDIO_SUPPORT_ACH_RINGTONE:?}\""
-        echo "SOURCE_AUDIO_SUPPORT_DUAL_SPEAKER=\"${SOURCE_AUDIO_SUPPORT_DUAL_SPEAKER:?}\""
-        echo "TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=\"${TARGET_AUDIO_SUPPORT_DUAL_SPEAKER:?}\""
-        echo "SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION=\"${SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION:?}\""
-        echo "TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=\"${TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION:?}\""
-        echo "SOURCE_AUTO_BRIGHTNESS_TYPE=\"${SOURCE_AUTO_BRIGHTNESS_TYPE:?}\""
-        echo "TARGET_AUTO_BRIGHTNESS_TYPE=\"${TARGET_AUTO_BRIGHTNESS_TYPE:?}\""
-        echo "SOURCE_DVFS_CONFIG_NAME=\"${SOURCE_DVFS_CONFIG_NAME:?}\""
-        echo "TARGET_DVFS_CONFIG_NAME=\"${TARGET_DVFS_CONFIG_NAME:?}\""
-        echo "SOURCE_ESE_CHIP_VENDOR=\"${SOURCE_ESE_CHIP_VENDOR:=none}\""
-        echo "TARGET_ESE_CHIP_VENDOR=\"${TARGET_ESE_CHIP_VENDOR:=none}\""
-        echo "SOURCE_ESE_COS_NAME=\"${SOURCE_ESE_COS_NAME:=none}\""
-        echo "TARGET_ESE_COS_NAME=\"${TARGET_ESE_COS_NAME:=none}\""
-        echo "SOURCE_FP_SENSOR_CONFIG=\"${SOURCE_FP_SENSOR_CONFIG:?}\""
-        echo "TARGET_FP_SENSOR_CONFIG=\"${TARGET_FP_SENSOR_CONFIG:?}\""
-        echo "SOURCE_HAS_HW_MDNIE=\"${SOURCE_HAS_HW_MDNIE:?}\""
-        echo "TARGET_HAS_HW_MDNIE=\"${TARGET_HAS_HW_MDNIE:?}\""
-        echo "SOURCE_HAS_MASS_CAMERA_APP=\"${SOURCE_HAS_MASS_CAMERA_APP:?}\""
-        echo "TARGET_HAS_MASS_CAMERA_APP=\"${TARGET_HAS_MASS_CAMERA_APP:?}\""
-        echo "SOURCE_HAS_QHD_DISPLAY=\"${SOURCE_HAS_QHD_DISPLAY:=?}\""
-        echo "TARGET_HAS_QHD_DISPLAY=\"${TARGET_HAS_QHD_DISPLAY:=?}\""
-        echo "SOURCE_HFR_MODE=\"${SOURCE_HFR_MODE:=none}\""
-        echo "TARGET_HFR_MODE=\"${TARGET_HFR_MODE:=none}\""
-        echo "SOURCE_HFR_SUPPORTED_REFRESH_RATE=\"${SOURCE_HFR_SUPPORTED_REFRESH_RATE:=none}\""
-        echo "TARGET_HFR_SUPPORTED_REFRESH_RATE=\"${TARGET_HFR_SUPPORTED_REFRESH_RATE:=none}\""
-        echo "SOURCE_HFR_DEFAULT_REFRESH_RATE=\"${SOURCE_HFR_DEFAULT_REFRESH_RATE:=none}\""
-        echo "TARGET_HFR_DEFAULT_REFRESH_RATE=\"${TARGET_HFR_DEFAULT_REFRESH_RATE:=none}\""
-        echo "SOURCE_HFR_SEAMLESS_BRT=\"${SOURCE_HFR_SEAMLESS_BRT:=none}\""
-        echo "TARGET_HFR_SEAMLESS_BRT=\"${TARGET_HFR_SEAMLESS_BRT:=none}\""
-        echo "SOURCE_HFR_SEAMLESS_LUX=\"${SOURCE_HFR_SEAMLESS_LUX:=none}\""
-        echo "TARGET_HFR_SEAMLESS_LUX=\"${TARGET_HFR_SEAMLESS_LUX:=none}\""
-        echo "SOURCE_IS_ESIM_SUPPORTED=\"${SOURCE_IS_ESIM_SUPPORTED:?}\""
-        echo "TARGET_IS_ESIM_SUPPORTED=\"${TARGET_IS_ESIM_SUPPORTED:?}\""
-        echo "SOURCE_MDNIE_SUPPORTED_MODES=\"${SOURCE_MDNIE_SUPPORTED_MODES:?}\""
-        echo "TARGET_MDNIE_SUPPORTED_MODES=\"${TARGET_MDNIE_SUPPORTED_MODES:?}\""
-        echo "SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION=\"${SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION:?}\""
-        echo "TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION=\"${TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION:?}\""
-        echo "SOURCE_MDNIE_SUPPORT_HDR_EFFECT=\"$(test "$((SOURCE_MDNIE_SUPPORTED_MODES & 4))" != "0" && echo "true" || echo "false")\""
-        echo "TARGET_MDNIE_SUPPORT_HDR_EFFECT=\"$(test "$((TARGET_MDNIE_SUPPORTED_MODES & 4))" != "0" && echo "true" || echo "false")\""
-        echo "SOURCE_MULTI_MIC_MANAGER_VERSION=\"${SOURCE_MULTI_MIC_MANAGER_VERSION:?}\""
-        echo "TARGET_MULTI_MIC_MANAGER_VERSION=\"${TARGET_MULTI_MIC_MANAGER_VERSION:?}\""
-        echo "SOURCE_SSRM_CONFIG_NAME=\"${SOURCE_SSRM_CONFIG_NAME:?}\""
-        echo "TARGET_SSRM_CONFIG_NAME=\"${TARGET_SSRM_CONFIG_NAME:?}\""
-        echo "SOURCE_SUPPORT_CUTOUT_PROTECTION=\"${SOURCE_SUPPORT_CUTOUT_PROTECTION:=false}\""
-        echo "TARGET_SUPPORT_CUTOUT_PROTECTION=\"${TARGET_SUPPORT_CUTOUT_PROTECTION:=false}\""
-    } > "$OUT_DIR/config.sh"
-}
-
-source "$SRC_DIR/target/$1/config.sh"
-source "$SRC_DIR/unica/config.sh"
 # ]
 
-GEN_CONFIG_FILE
-[ -f "$OUT_DIR/config.sh" ] || exit 1
+if [ $# -ne 1 ]; then
+    echo "Usage: gen_config_file <target>" >&2
+    exit 1
+elif [ ! -f "$SRC_DIR/target/$1/config.sh" ]; then
+    LOGE "File not found: target/$1/config.sh"
+    exit 1
+fi
+
+source "$SRC_DIR/target/$1/config.sh" || exit 1
+source "$SRC_DIR/unica/config.sh" || exit 1
+
+if [ -f "$OUT_DIR/config.sh" ]; then
+    LOGW "config.sh already exists. Regenerating"
+    rm -f "$OUT_DIR/config.sh"
+fi
+
+{
+    echo "# Automatically generated by scripts/internal/gen_config_file.sh"
+    echo "ROM_IS_OFFICIAL=\"$(IS_UNICA_CERT_AVAILABLE)\""
+    GET_BUILD_VAR "ROM_VERSION"
+    GET_BUILD_VAR "ROM_CODENAME"
+    GET_BUILD_VAR "ROM_BUILD_TIMESTAMP" "$(date +%s)"
+    GET_BUILD_VAR "SOURCE_FIRMWARE"
+    if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
+        echo "SOURCE_EXTRA_FIRMWARES=\"$(IFS=":"; printf '%s' "${SOURCE_EXTRA_FIRMWARES[*]}")\""
+    else
+        echo "SOURCE_EXTRA_FIRMWARES=\"\""
+    fi
+    GET_BUILD_VAR "SOURCE_API_LEVEL"
+    GET_BUILD_VAR "SOURCE_PRODUCT_FIRST_API_LEVEL"
+    GET_BUILD_VAR "SOURCE_VNDK_VERSION"
+    GET_BUILD_VAR "TARGET_NAME"
+    GET_BUILD_VAR "TARGET_CODENAME"
+    if [ "${#TARGET_ASSERT_MODEL[@]}" -ge 1 ]; then
+        echo "TARGET_ASSERT_MODEL=\"$(IFS=":"; printf '%s' "${TARGET_ASSERT_MODEL[*]}")\""
+    else
+        echo "TARGET_ASSERT_MODEL=\"\""
+    fi
+    GET_BUILD_VAR "TARGET_FIRMWARE"
+    if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
+        echo "TARGET_EXTRA_FIRMWARES=\"$(IFS=":"; printf '%s' "${TARGET_EXTRA_FIRMWARES[*]}")\""
+    else
+        echo "TARGET_EXTRA_FIRMWARES=\"\""
+    fi
+    GET_BUILD_VAR "TARGET_API_LEVEL"
+    GET_BUILD_VAR "TARGET_PRODUCT_FIRST_API_LEVEL"
+    GET_BUILD_VAR "TARGET_VNDK_VERSION"
+    GET_BUILD_VAR "TARGET_SINGLE_SYSTEM_IMAGE"
+    GET_BUILD_VAR "TARGET_OS_FILE_SYSTEM"
+    GET_BUILD_VAR "TARGET_BOOT_DEVICE_PATH" "/dev/block/bootdevice/by-name"
+    GET_BUILD_VAR "TARGET_INCLUDE_PATCHED_VBMETA" "false"
+    GET_BUILD_VAR "TARGET_KEEP_ORIGINAL_SIGN" "false"
+    GET_BUILD_VAR "TARGET_SUPER_PARTITION_SIZE"
+    GET_BUILD_VAR "SOURCE_SUPER_GROUP_NAME"
+    GET_BUILD_VAR "TARGET_SUPER_GROUP_NAME" "$SOURCE_SUPER_GROUP_NAME"
+    GET_BUILD_VAR "TARGET_SUPER_GROUP_SIZE"
+    GET_BUILD_VAR "SOURCE_HAS_SYSTEM_EXT"
+    GET_BUILD_VAR "TARGET_HAS_SYSTEM_EXT"
+    GET_BUILD_VAR "SOURCE_AUDIO_SUPPORT_ACH_RINGTONE"
+    GET_BUILD_VAR "TARGET_AUDIO_SUPPORT_ACH_RINGTONE"
+    GET_BUILD_VAR "SOURCE_AUDIO_SUPPORT_DUAL_SPEAKER"
+    GET_BUILD_VAR "TARGET_AUDIO_SUPPORT_DUAL_SPEAKER"
+    GET_BUILD_VAR "SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION"
+    GET_BUILD_VAR "TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION"
+    GET_BUILD_VAR "SOURCE_AUTO_BRIGHTNESS_TYPE"
+    GET_BUILD_VAR "TARGET_AUTO_BRIGHTNESS_TYPE"
+    GET_BUILD_VAR "SOURCE_DVFS_CONFIG_NAME"
+    GET_BUILD_VAR "TARGET_DVFS_CONFIG_NAME"
+    GET_BUILD_VAR "SOURCE_ESE_CHIP_VENDOR" "none"
+    GET_BUILD_VAR "TARGET_ESE_CHIP_VENDOR" "none"
+    GET_BUILD_VAR "SOURCE_ESE_COS_NAME" "none"
+    GET_BUILD_VAR "TARGET_ESE_COS_NAME" "none"
+    GET_BUILD_VAR "SOURCE_FP_SENSOR_CONFIG"
+    GET_BUILD_VAR "TARGET_FP_SENSOR_CONFIG"
+    GET_BUILD_VAR "SOURCE_HAS_HW_MDNIE"
+    GET_BUILD_VAR "TARGET_HAS_HW_MDNIE"
+    GET_BUILD_VAR "SOURCE_HAS_MASS_CAMERA_APP"
+    GET_BUILD_VAR "TARGET_HAS_MASS_CAMERA_APP"
+    GET_BUILD_VAR "SOURCE_HAS_QHD_DISPLAY"
+    GET_BUILD_VAR "TARGET_HAS_QHD_DISPLAY"
+    GET_BUILD_VAR "SOURCE_HFR_MODE"
+    GET_BUILD_VAR "TARGET_HFR_MODE"
+    GET_BUILD_VAR "SOURCE_HFR_SUPPORTED_REFRESH_RATE" "none"
+    GET_BUILD_VAR "TARGET_HFR_SUPPORTED_REFRESH_RATE" "none"
+    GET_BUILD_VAR "SOURCE_HFR_DEFAULT_REFRESH_RATE" "none"
+    GET_BUILD_VAR "TARGET_HFR_DEFAULT_REFRESH_RATE" "none"
+    GET_BUILD_VAR "SOURCE_HFR_SEAMLESS_BRT" "none"
+    GET_BUILD_VAR "TARGET_HFR_SEAMLESS_BRT" "none"
+    GET_BUILD_VAR "SOURCE_HFR_SEAMLESS_LUX" "none"
+    GET_BUILD_VAR "TARGET_HFR_SEAMLESS_LUX" "none"
+    GET_BUILD_VAR "SOURCE_IS_ESIM_SUPPORTED"
+    GET_BUILD_VAR "TARGET_IS_ESIM_SUPPORTED"
+    GET_BUILD_VAR "SOURCE_MDNIE_SUPPORTED_MODES"
+    GET_BUILD_VAR "TARGET_MDNIE_SUPPORTED_MODES"
+    GET_BUILD_VAR "SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION"
+    GET_BUILD_VAR "TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION"
+    GET_BUILD_VAR "SOURCE_MDNIE_SUPPORT_HDR_EFFECT" "$(test "$((SOURCE_MDNIE_SUPPORTED_MODES & 4))" != "0" && echo "true" || echo "false")"
+    GET_BUILD_VAR "TARGET_MDNIE_SUPPORT_HDR_EFFECT" "$(test "$((TARGET_MDNIE_SUPPORTED_MODES & 4))" != "0" && echo "true" || echo "false")"
+    GET_BUILD_VAR "SOURCE_MULTI_MIC_MANAGER_VERSION"
+    GET_BUILD_VAR "TARGET_MULTI_MIC_MANAGER_VERSION"
+    GET_BUILD_VAR "SOURCE_SSRM_CONFIG_NAME"
+    GET_BUILD_VAR "TARGET_SSRM_CONFIG_NAME"
+    GET_BUILD_VAR "SOURCE_SUPPORT_CUTOUT_PROTECTION" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_CUTOUT_PROTECTION" "false"
+} > "$OUT_DIR/config.sh"
 
 exit 0

Ocin4ever/EternityROM

scripts/internal/gen_config_file.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2023 Salvo Giangreco
+# Copyright (C) 2025 Salvo Giangreco
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,107 +16,138 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# shellcheck disable=SC1090,SC1091
-
-set -Ee
-
 # [
-trap 'rm -f $OUT_DIR/config.sh' ERR
+source "$SRC_DIR/scripts/utils/common_utils.sh" || exit 1
 
-GEN_CONFIG_FILE()
+trap '[ $? -ne 0 ] && rm -f "$OUT_DIR/config.sh"' EXIT
+
+GET_BUILD_VAR()
 {
-    if [ -f "$OUT_DIR/config.sh" ]; then
-        echo "config.sh already exists. Regenerating..."
-        rm -f "$OUT_DIR/config.sh"
+    if [ "$2" ]; then
+        if [ ! "${!1}" ]; then
+            echo "${1}=\"${2}\""
+            return 0
+        fi
+    else
+        _CHECK_NON_EMPTY_PARAM "$1" "${!1}" || exit 1
     fi
 
-    {
-        echo "# Automatically generated by unica/scripts/internal/gen_config_file.sh"
-        echo "ROM_VERSION=\"${ROM_VERSION:?}\""
-        echo "DEBUG=\"${DEBUG:?}\""
-        echo "ROM_BUILD_TIMESTAMP=\"$(date '+%s')\""
-        echo "SOURCE_FIRMWARE=\"${SOURCE_FIRMWARE:?}\""
-        if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            echo "SOURCE_EXTRA_FIRMWARES=\"$( IFS=:; printf '%s' "${SOURCE_EXTRA_FIRMWARES[*]}" )\""
-        else
-            echo "SOURCE_EXTRA_FIRMWARES=\"\""
-        fi
-        echo "SOURCE_API_LEVEL=\"${SOURCE_API_LEVEL:?}\""
-        echo "SOURCE_PRODUCT_FIRST_API_LEVEL=\"${SOURCE_PRODUCT_FIRST_API_LEVEL:?}\""
-        echo "SOURCE_VNDK_VERSION=\"${SOURCE_VNDK_VERSION:?}\""
-        echo "TARGET_NAME=\"${TARGET_NAME:?}\""
-        echo "SOURCE_CODENAME=\"${SOURCE_CODENAME:?}\""
-        echo "TARGET_CODENAME=\"${TARGET_CODENAME:?}\""
-        echo "TARGET_PLATFORM=\"${TARGET_PLATFORM:?}\""
-        if [ "${#TARGET_ASSERT_MODEL[@]}" -ge 1 ]; then
-            echo "TARGET_ASSERT_MODEL=\"$( IFS=:; printf '%s' "${TARGET_ASSERT_MODEL[*]}" )\""
-        else
-            echo "TARGET_ASSERT_MODEL=\"\""
-        fi
-        echo "TARGET_FIRMWARE=\"${TARGET_FIRMWARE:?}\""
-        if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
-            echo "TARGET_EXTRA_FIRMWARES=\"$( IFS=:; printf '%s' "${TARGET_EXTRA_FIRMWARES[*]}" )\""
-        else
-            echo "TARGET_EXTRA_FIRMWARES=\"\""
-        fi
-        echo "TARGET_API_LEVEL=\"${TARGET_API_LEVEL:?}\""
-        echo "TARGET_PRODUCT_FIRST_API_LEVEL=\"${TARGET_PRODUCT_FIRST_API_LEVEL:?}\""
-        echo "TARGET_VNDK_VERSION=\"${TARGET_VNDK_VERSION:?}\""
-        echo "TARGET_SINGLE_SYSTEM_IMAGE=\"${TARGET_SINGLE_SYSTEM_IMAGE:?}\""
-        echo "TARGET_OS_FILE_SYSTEM=\"${TARGET_OS_FILE_SYSTEM:?}\""
-        echo "TARGET_BOOT_DEVICE_PATH=\"${TARGET_BOOT_DEVICE_PATH:=/dev/block/bootdevice/by-name}\""
-        echo "TARGET_INCLUDE_PATCHED_VBMETA=\"${TARGET_INCLUDE_PATCHED_VBMETA:=false}\""
-        echo "TARGET_KEEP_ORIGINAL_SIGN=\"${TARGET_KEEP_ORIGINAL_SIGN:=false}\""
-        echo "TARGET_SUPER_PARTITION_SIZE=\"${TARGET_SUPER_PARTITION_SIZE:?}\""
-        echo "SOURCE_SUPER_GROUP_NAME=\"${SOURCE_SUPER_GROUP_NAME:?}\""
-        echo "TARGET_SUPER_GROUP_NAME=\"${TARGET_SUPER_GROUP_NAME:=$SOURCE_SUPER_GROUP_NAME}\""
-        echo "TARGET_SUPER_GROUP_SIZE=\"${TARGET_SUPER_GROUP_SIZE:?}\""
-        echo "SOURCE_HAS_SYSTEM_EXT=\"${SOURCE_HAS_SYSTEM_EXT:?}\""
-        echo "TARGET_HAS_SYSTEM_EXT=\"${TARGET_HAS_SYSTEM_EXT:?}\""
-        echo "SOURCE_AUTO_BRIGHTNESS_TYPE=\"${SOURCE_AUTO_BRIGHTNESS_TYPE:?}\""
-        echo "TARGET_AUTO_BRIGHTNESS_TYPE=\"${TARGET_AUTO_BRIGHTNESS_TYPE:?}\""
-        echo "SOURCE_DVFS_CONFIG_NAME=\"${SOURCE_DVFS_CONFIG_NAME:?}\""
-        echo "TARGET_DVFS_CONFIG_NAME=\"${TARGET_DVFS_CONFIG_NAME:?}\""
-        echo "SOURCE_NFC_CHIP_VENDOR=\"${SOURCE_NFC_CHIP_VENDOR:=none}\""
-        echo "TARGET_NFC_CHIP_VENDOR=\"${TARGET_NFC_CHIP_VENDOR:=none}\""
-        echo "SOURCE_FP_SENSOR_CONFIG=\"${SOURCE_FP_SENSOR_CONFIG:?}\""
-        echo "TARGET_FP_SENSOR_CONFIG=\"${TARGET_FP_SENSOR_CONFIG:?}\""
-        echo "SOURCE_HAS_MASS_CAMERA_APP=\"${SOURCE_HAS_MASS_CAMERA_APP:?}\""
-        echo "TARGET_HAS_MASS_CAMERA_APP=\"${TARGET_HAS_MASS_CAMERA_APP:?}\""
-        echo "SOURCE_HAS_QHD_DISPLAY=\"${SOURCE_HAS_QHD_DISPLAY:=?}\""
-        echo "TARGET_HAS_QHD_DISPLAY=\"${TARGET_HAS_QHD_DISPLAY:=?}\""
-        echo "SOURCE_HFR_MODE=\"${SOURCE_HFR_MODE:=none}\""
-        echo "TARGET_HFR_MODE=\"${TARGET_HFR_MODE:=none}\""
-        echo "SOURCE_HFR_SUPPORTED_REFRESH_RATE=\"${SOURCE_HFR_SUPPORTED_REFRESH_RATE:=none}\""
-        echo "TARGET_HFR_SUPPORTED_REFRESH_RATE=\"${TARGET_HFR_SUPPORTED_REFRESH_RATE:=none}\""
-        echo "SOURCE_HFR_DEFAULT_REFRESH_RATE=\"${SOURCE_HFR_DEFAULT_REFRESH_RATE:=none}\""
-        echo "TARGET_HFR_DEFAULT_REFRESH_RATE=\"${TARGET_HFR_DEFAULT_REFRESH_RATE:=none}\""
-        echo "SOURCE_DISPLAY_CUTOUT_TYPE=\"${SOURCE_DISPLAY_CUTOUT_TYPE:=none}\""
-        echo "TARGET_DISPLAY_CUTOUT_TYPE=\"${TARGET_DISPLAY_CUTOUT_TYPE:=none}\""
-        echo "SOURCE_IS_ESIM_SUPPORTED=\"${SOURCE_IS_ESIM_SUPPORTED:?}\""
-        echo "TARGET_IS_ESIM_SUPPORTED=\"${TARGET_IS_ESIM_SUPPORTED:?}\""
-        echo "SOURCE_MDNIE_SUPPORTED_MODES=\"${SOURCE_MDNIE_SUPPORTED_MODES:?}\""
-        echo "TARGET_MDNIE_SUPPORTED_MODES=\"${TARGET_MDNIE_SUPPORTED_MODES:?}\""
-        echo "SOURCE_SUPPORT_WIFI_7=\"${SOURCE_SUPPORT_WIFI_7:=false}\""
-        echo "TARGET_SUPPORT_WIFI_7=\"${TARGET_SUPPORT_WIFI_7:=false}\""
-        echo "SOURCE_SUPPORT_HOTSPOT_DUALAP=\"${SOURCE_SUPPORT_HOTSPOT_DUALAP:=false}\""
-        echo "TARGET_SUPPORT_HOTSPOT_DUALAP=\"${TARGET_SUPPORT_HOTSPOT_DUALAP:=false}\""
-        echo "SOURCE_SUPPORT_HOTSPOT_WPA3=\"${SOURCE_SUPPORT_HOTSPOT_WPA3:=false}\""
-        echo "TARGET_SUPPORT_HOTSPOT_WPA3=\"${TARGET_SUPPORT_HOTSPOT_WPA3:=false}\""
-        echo "SOURCE_SUPPORT_HOTSPOT_6GHZ=\"${SOURCE_SUPPORT_HOTSPOT_6GHZ:=false}\""
-        echo "TARGET_SUPPORT_HOTSPOT_6GHZ=\"${TARGET_SUPPORT_HOTSPOT_6GHZ:=false}\""
-        echo "SOURCE_SUPPORT_HOTSPOT_WIFI_6=\"${SOURCE_SUPPORT_HOTSPOT_WIFI_6:=false}\""
-        echo "TARGET_SUPPORT_HOTSPOT_WIFI_6=\"${TARGET_SUPPORT_HOTSPOT_WIFI_6:=false}\""
-        echo "SOURCE_SUPPORT_HOTSPOT_ENHANCED_OPEN=\"${SOURCE_SUPPORT_HOTSPOT_ENHANCED_OPEN:=false}\""
-        echo "TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN=\"${TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN:=false}\""
-    } > "$OUT_DIR/config.sh"
+    echo "${1}=\"${!1}\""
+    return 0
 }
 
-source "$SRC_DIR/target/$1/config.sh"
-source "$SRC_DIR/unica/config.sh"
+IS_UNICA_CERT_AVAILABLE()
+{
+    local PLATFORM_KEY_SHA1="5b0eb951718acc596370dabab83f546e779b21dc"
+    local OTA_KEY_SHA1="681aa9d28fe5fc60be8c25dc5f26a73ec3d6fb46"
+
+    local USES_UNICA_CERT="false"
+    if [[ "$(sha1sum "$SRC_DIR/security/unica_platform.pk8" 2> /dev/null | cut -d " " -f 1)" == "$PLATFORM_KEY_SHA1" ]] && \
+            [[ "$(sha1sum "$SRC_DIR/security/unica_ota.pk8" 2> /dev/null | cut -d " " -f 1)" == "$OTA_KEY_SHA1" ]]; then
+        USES_UNICA_CERT="true"
+    fi
+
+    echo "$USES_UNICA_CERT"
+}
 # ]
 
-GEN_CONFIG_FILE
-[ -f "$OUT_DIR/config.sh" ] || exit 1
+if [ $# -ne 1 ]; then
+    echo "Usage: gen_config_file <target>" >&2
+    exit 1
+elif [ ! -f "$SRC_DIR/target/$1/config.sh" ]; then
+    LOGE "File not found: target/$1/config.sh"
+    exit 1
+fi
+
+source "$SRC_DIR/target/$1/config.sh" || exit 1
+source "$SRC_DIR/unica/config.sh" || exit 1
+
+if [ -f "$OUT_DIR/config.sh" ]; then
+    LOGW "config.sh already exists. Regenerating"
+    rm -f "$OUT_DIR/config.sh"
+fi
+
+{
+    echo "# Automatically generated by scripts/internal/gen_config_file.sh"
+    GET_BUILD_VAR "ROM_VERSION"
+    GET_BUILD_VAR "DEBUG"
+    GET_BUILD_VAR "ROM_BUILD_TIMESTAMP" "$(date +%s)"
+    GET_BUILD_VAR "SOURCE_FIRMWARE"
+    if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
+        echo "SOURCE_EXTRA_FIRMWARES=\"$(IFS=":"; printf '%s' "${SOURCE_EXTRA_FIRMWARES[*]}")\""
+    else
+        echo "SOURCE_EXTRA_FIRMWARES=\"\""
+    fi
+    GET_BUILD_VAR "SOURCE_API_LEVEL"
+    GET_BUILD_VAR "SOURCE_PRODUCT_FIRST_API_LEVEL"
+    GET_BUILD_VAR "SOURCE_VNDK_VERSION"
+    GET_BUILD_VAR "TARGET_NAME"
+    GET_BUILD_VAR "SOURCE_CODENAME"
+    GET_BUILD_VAR "TARGET_CODENAME"
+    GET_BUILD_VAR "TARGET_PLATFORM"
+    if [ "${#TARGET_ASSERT_MODEL[@]}" -ge 1 ]; then
+        echo "TARGET_ASSERT_MODEL=\"$(IFS=":"; printf '%s' "${TARGET_ASSERT_MODEL[*]}")\""
+    else
+        echo "TARGET_ASSERT_MODEL=\"\""
+    fi
+    GET_BUILD_VAR "TARGET_FIRMWARE"
+    if [ "${#TARGET_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
+        echo "TARGET_EXTRA_FIRMWARES=\"$(IFS=":"; printf '%s' "${TARGET_EXTRA_FIRMWARES[*]}")\""
+    else
+        echo "TARGET_EXTRA_FIRMWARES=\"\""
+    fi
+    GET_BUILD_VAR "TARGET_API_LEVEL"
+    GET_BUILD_VAR "TARGET_PRODUCT_FIRST_API_LEVEL"
+    GET_BUILD_VAR "TARGET_VNDK_VERSION"
+    GET_BUILD_VAR "TARGET_SINGLE_SYSTEM_IMAGE"
+    GET_BUILD_VAR "TARGET_OS_FILE_SYSTEM"
+    GET_BUILD_VAR "TARGET_BOOT_DEVICE_PATH" "/dev/block/bootdevice/by-name"
+    GET_BUILD_VAR "TARGET_INCLUDE_PATCHED_VBMETA" "false"
+    GET_BUILD_VAR "TARGET_KEEP_ORIGINAL_SIGN" "false"
+    GET_BUILD_VAR "TARGET_SUPER_PARTITION_SIZE"
+    GET_BUILD_VAR "SOURCE_SUPER_GROUP_NAME"
+    GET_BUILD_VAR "TARGET_SUPER_GROUP_NAME" "$SOURCE_SUPER_GROUP_NAME"
+    GET_BUILD_VAR "TARGET_SUPER_GROUP_SIZE"
+    GET_BUILD_VAR "SOURCE_HAS_SYSTEM_EXT"
+    GET_BUILD_VAR "TARGET_HAS_SYSTEM_EXT"
+    GET_BUILD_VAR "SOURCE_AUDIO_SUPPORT_ACH_RINGTONE"
+    GET_BUILD_VAR "TARGET_AUDIO_SUPPORT_ACH_RINGTONE"
+    GET_BUILD_VAR "SOURCE_AUTO_BRIGHTNESS_TYPE"
+    GET_BUILD_VAR "TARGET_AUTO_BRIGHTNESS_TYPE"
+    GET_BUILD_VAR "SOURCE_DVFS_CONFIG_NAME"
+    GET_BUILD_VAR "TARGET_DVFS_CONFIG_NAME"
+    GET_BUILD_VAR "SOURCE_NFC_CHIP_VENDOR" "none"
+    GET_BUILD_VAR "TARGET_NFC_CHIP_VENDOR" "none"
+    GET_BUILD_VAR "SOURCE_FP_SENSOR_CONFIG"
+    GET_BUILD_VAR "TARGET_FP_SENSOR_CONFIG"
+    GET_BUILD_VAR "SOURCE_HAS_MASS_CAMERA_APP"
+    GET_BUILD_VAR "TARGET_HAS_MASS_CAMERA_APP"
+    GET_BUILD_VAR "SOURCE_HAS_QHD_DISPLAY"
+    GET_BUILD_VAR "TARGET_HAS_QHD_DISPLAY"
+    GET_BUILD_VAR "SOURCE_HFR_MODE" "none"
+    GET_BUILD_VAR "TARGET_HFR_MODE" "none"
+    GET_BUILD_VAR "SOURCE_HFR_SUPPORTED_REFRESH_RATE" "none"
+    GET_BUILD_VAR "TARGET_HFR_SUPPORTED_REFRESH_RATE" "none"
+    GET_BUILD_VAR "SOURCE_HFR_DEFAULT_REFRESH_RATE" "none"
+    GET_BUILD_VAR "TARGET_HFR_DEFAULT_REFRESH_RATE" "none"
+    GET_BUILD_VAR "SOURCE_DISPLAY_CUTOUT_TYPE" "none"
+    GET_BUILD_VAR "TARGET_DISPLAY_CUTOUT_TYPE" "none"
+    GET_BUILD_VAR "SOURCE_IS_ESIM_SUPPORTED"
+    GET_BUILD_VAR "TARGET_IS_ESIM_SUPPORTED"
+    GET_BUILD_VAR "SOURCE_MDNIE_SUPPORTED_MODES"
+    GET_BUILD_VAR "TARGET_MDNIE_SUPPORTED_MODES"
+    GET_BUILD_VAR "SOURCE_SUPPORT_WIFI_7" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_WIFI_7" "false"
+    GET_BUILD_VAR "SOURCE_SUPPORT_HOTSPOT_DUALAP" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_HOTSPOT_DUALAP" "false"
+    GET_BUILD_VAR "SOURCE_SUPPORT_HOTSPOT_WPA3" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_HOTSPOT_WPA3" "false"
+    GET_BUILD_VAR "SOURCE_SUPPORT_HOTSPOT_6GHZ" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_HOTSPOT_6GHZ" "false"
+    GET_BUILD_VAR "SOURCE_SUPPORT_HOTSPOT_WIFI_6" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_HOTSPOT_WIFI_6" "false"
+    GET_BUILD_VAR "SOURCE_SUPPORT_HOTSPOT_ENHANCED_OPEN" "false"
+    GET_BUILD_VAR "TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN" "false"
+} > "$OUT_DIR/config.sh"
 
 exit 0