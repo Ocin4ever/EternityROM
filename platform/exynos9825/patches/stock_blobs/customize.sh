fd6c22e00d8afca7ee937472dc42e9d2a0b7be16
Ocin4ever <85343782+Ocin4ever@users.noreply.github.com>
2025-07-08 13:43:32 +0200

    platform: exynos9825: patches: stock_blobs: customize.sh: add missing config to match recent changes

--- platform/exynos9825/patches/stock_blobs/customize.sh
+++ platform/exynos9825/patches/stock_blobs/customize.sh
@@ -1,4 +1,4 @@
-# S24+ OneUI 7 -> SoundBooster 2000
+# S24 FE OneUI 7 -> SoundBooster 2000
 # N10 Series -> SoundBooster 1050
 echo "Replacing SoundBooster"
 DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
@@ -6,7 +6,6 @@ ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0
 ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 echo "Replacing GameDriver"
-DELETE_FROM_WORK_DIR "system" "system/priv-app/GameDriver-EX2400"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/GameDriver-EX9825/GameDriver-EX9825.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/DevGPUDriver-EX9825/DevGPUDriver-EX9825.apk" 0 0 644 "u:object_r:system_file:s0"
 
@@ -26,25 +25,9 @@ SET_METADATA "product" "priv-app/HotwordEnrollmentOKGoogleExCORTEXM4/HotwordEnro
 SET_METADATA "product" "priv-app/HotwordEnrollmentXGoogleExCORTEXM4" 0 0 755 "u:object_r:system_file:s0"
 SET_METADATA "product" "priv-app/HotwordEnrollmentXGoogleExCORTEXM4/HotwordEnrollmentXGoogleExCORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
 
-echo "Downgrading VaultKeeper JNI"
-ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib64/libvkjni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib64/libvkmanager.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib64/vendor.samsung.hardware.security.vaultkeeper@2.0.so" 0 0 644 "u:object_r:system_lib_file:s0"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.vaultkeeper-V1-ndk.so"
-
-echo "Downgrading ENGMODE JNI"
-ADD_TO_WORK_DIR "dm3qxxx" "system" "lib64/lib.engmode.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "dm3qxxx" "system" "lib64/lib.engmodejni.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "dm3qxxx" "system" "lib64/vendor.samsung.hardware.security.engmode@1.0.so" 0 0 644 "u:object_r:system_lib_file:s0"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.engmode-V1-ndk.so"
-
 echo "Adding 32-Bit WFD blobs"
 ADD_TO_WORK_DIR "p3sxxx" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
 ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib" 0 0 644 "u:object_r:system_lib_file:s0"
-
-echo "Adding LED Case Cover support"
-ADD_TO_WORK_DIR "p3sxxx" "system" "system/priv-app/LedCoverService/LedCoverService.apk"
-ADD_TO_WORK_DIR "p3sxxx" "system" "system/etc/permissions/privapp-permissions-com.sec.android.cover.ledcover.xml"
 
 echo "Adding stock NFC Case features"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/com.sec.feature.cover.clearsideviewcover.xml" 0 0 644 "u:object_r:system_file:s0"
