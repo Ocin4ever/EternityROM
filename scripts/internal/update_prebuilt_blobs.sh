16e699678d44f282f288403df7ac567ccb2c8e53
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-08-16 12:59:21 +0300

    scripts: internal: update_prebuilts_blobs: fix double extraction
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- scripts/internal/update_prebuilt_blobs.sh
+++ scripts/internal/update_prebuilt_blobs.sh
@@ -114,8 +114,8 @@ export SOURCE_FIRMWARE="$FIRMWARE"
 export TARGET_FIRMWARE="$FIRMWARE"
 export SOURCE_EXTRA_FIRMWARES=""
 export TARGET_EXTRA_FIRMWARES=""
-"$SRC_DIR/scripts/download_fw.sh"
-"$SRC_DIR/scripts/extract_fw.sh"
+"$SRC_DIR/scripts/download_fw.sh" --force --ignore-target
+"$SRC_DIR/scripts/extract_fw.sh" --force --ignore-target
 
 for i in $BLOBS; do
     if [[ "$i" == *[0-9] ]]; then
