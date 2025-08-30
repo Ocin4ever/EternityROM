alessandroaraujo5/ExtremeROM-Revived

unica: patches: signature: fixup
Change-Id: I85f4fe068b65a5cd60d2303cead907c1f5dfb386
Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

unica/patches/signature/customize.sh
@@ -1,12 +1,8 @@
+APPLY_PATCH "system" "system/framework/services.jar" "$MODPATH/0001-Allow-custom-platform-signature.patch"
+
 CERT_PREFIX="aosp"
-$ROM_IS_OFFICIAL && CERT_PREFIX="unica"
+$ROM_IS_OFFICIAL && CERT_PREFIX="extremerom"
 
-CERT_SIGNATURE=$(cat "$SRC_DIR/security/${CERT_PREFIX}.x509.pem" | \
-    sed '/CERTIFICATE/d' | tr -d '\n' | base64 -d | xxd -p -c 0)
+CERT_SIGNATURE=$(cat "$SRC_DIR/security/${CERT_PREFIX}_platform.x509.pem" | sed '/CERTIFICATE/d' | tr -d '\n' | base64 -d | xxd -p -c 0)
 
-FTP="
-system/framework/services.jar/smali_classes2/com/android/server/pm/InstallPackageHelper.smali
-"
-for f in $FTP; do
-    sed -i "s|PUT SIGNATURE HERE|$CERT_SIGNATURE|g" "$APKTOOL_DIR/$f"
-done
+sed -i "s|PUT SIGNATURE HERE|$CERT_SIGNATURE|g" "$APKTOOL_DIR/system/framework/services.jar/smali_classes2/com/android/server/pm/InstallPackageHelper.smali"

Ocin4ever/EternityROM

unica/patches/signature/customize.sh
@@ -1,12 +1,8 @@
+APPLY_PATCH "system" "system/framework/services.jar" "$MODPATH/0001-Allow-custom-platform-signature.patch"
+
 CERT_PREFIX="aosp"
-$ROM_IS_OFFICIAL && CERT_PREFIX="unica"
+$ROM_IS_OFFICIAL && CERT_PREFIX="eternityrom"
 
-CERT_SIGNATURE=$(cat "$SRC_DIR/security/${CERT_PREFIX}.x509.pem" | \
-    sed '/CERTIFICATE/d' | tr -d '\n' | base64 -d | xxd -p -c 0)
+CERT_SIGNATURE=$(cat "$SRC_DIR/security/${CERT_PREFIX}_platform.x509.pem" | sed '/CERTIFICATE/d' | tr -d '\n' | base64 -d | xxd -p -c 0)
 
-FTP="
-system/framework/services.jar/smali_classes2/com/android/server/pm/InstallPackageHelper.smali
-"
-for f in $FTP; do
-    sed -i "s|PUT SIGNATURE HERE|$CERT_SIGNATURE|g" "$APKTOOL_DIR/$f"
-done
+sed -i "s|PUT SIGNATURE HERE|$CERT_SIGNATURE|g" "$APKTOOL_DIR/system/framework/services.jar/smali_classes2/com/android/server/pm/InstallPackageHelper.smali"