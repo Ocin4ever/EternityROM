alessandroaraujo5/ExtremeROM-Revived

unica: patches: miscs: remove OFFICIAL for official builds
Change-Id: I3abec3090ae45624dc9b486cc074bcb2fc865a6d
Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

unica/patches/miscs/customize.sh
@@ -2,12 +2,12 @@ MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
 REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 # Set build ID
-ROM_STATUS="UNOFFICIAL"
-$ROM_IS_OFFICIAL && ROM_STATUS="OFFICIAL"
+ROM_STATUS=""
+$ROM_IS_OFFICIAL || ROM_STATUS=" UNOFFICIAL"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "ExtremeROM $ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
+SET_PROP "system" "ro.build.display.id" "ExtremeROM$ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
 
-SET_PROP "system" "ro.extremerom.status" "$ROM_STATUS"
+SET_PROP "system" "ro.extremerom.official" "$ROM_IS_OFFICIAL"
 SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
 SET_PROP "system" "ro.extremerom.codename" "$ROM_CODENAME"
 

Ocin4ever/EternityROM

unica/patches/miscs/customize.sh
@@ -2,12 +2,13 @@ MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
 REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 # Set build ID
-# Nobody is allowed to remove this part and distribute it
-ROM_STATUS=" (UNOFFICIAL)"
-$ROM_IS_OFFICIAL && ROM_STATUS=""
+# You are not allowed to remove this part and distribute it
+ROM_STATUS=""
+$ROM_IS_OFFICIAL || ROM_STATUS=" UNOFFICIAL"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "EternityROM $ROM_VERSION$ROM_STATUS $ROM_COMMIT ($VALUE)"
+SET_PROP "system" "ro.build.display.id" "EternityROM $ROM_VERSION $ROM_COMMIT ($VALUE)"
 
+SET_PROP "system" "ro.eternityrom.official" "$ROM_IS_OFFICIAL"
 SET_PROP "system" "ro.eternityrom.version" "$ROM_VERSION"
 
 # Disable FRP